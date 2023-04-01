c49c190293c34a506de869d2078ae56377b420eb
KrisMarboe <kristoffermarboe@gmail.com>
2023-04-01 13:24:27 +0200

    Included pythonanywhere specific database stuff in app.py

--- app.py
+++ app.py
@@ -16,14 +16,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 with app.app_context():
     db = SQLAlchemy(app)
 
-controls = {
-    "Escape": "Reset graph",
-    "Backspace": "Undo course selection",
-    "Scroll": "Zoom in/out",
-    "Double click": "Create focused network for selected course",
-    "Click + Drag": "Move the network",
-    "Ctrl + Click": "Open course page in new tab"
-}
 
 class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,6 +24,42 @@ class Department(db.Model):
 
     def __repr__(self):
         return f'<Department {self.name}>'
+
+"""
+For the pythonanywhere website
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="krism",
+    password="",
+    hostname="krism.mysql.pythonanywhere-services.com",
+    databasename="krism$department",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Department(db.Model):
+
+    __tablename__ = "departments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    color = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return f'<Department {self.name}>'
+
+"""
+
+controls = {
+    "Escape": "Reset graph",
+    "Backspace": "Undo course selection",
+    "Scroll": "Zoom in/out",
+    "Double click": "Create focused network for selected course",
+    "Click + Drag": "Move the network",
+    "Ctrl + Click": "Open course page in new tab"
+}
 
 
 @app.route("/", methods=['POST', 'GET'])
